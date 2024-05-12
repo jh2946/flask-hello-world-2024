be955621d2b149205e87ccf11ea4466d2b9785f0
jh2946 <junhong2946@gmail.com>
2024-05-12 08:27:26 +0800

    link to lambda

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def home(): return 'hi'
+def home(): return 'hi, here\'s <a href="/lambda">lambda</a>'
 @app.route('/about')
 def about(): return 'about'
 app.run(host='0.0.0.0', port=80)
